9aa762d035b112bdc51b85aea7097a220504dbb4
daniestrijdom <daansteraan@gmail.com>
2016-09-13 21:38:00 +0200

    wrangling data- final

--- Module2/assignment5.py
+++ Module2/assignment5.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 
@@ -11,13 +12,22 @@ import numpy as np
 #
 # .. your code here ..
 
+headers = ['a','education', 'age', 'capital-gain', 'race', 'capital-loss', 'hours-per-week', 'sex', 'classification']
+df = pd.read_csv("Datasets\census.data", header=None)
+df.columns = headers
+df = df.drop('a',1)
+
+df.fillna(np.nan)
 
 # TODO:
 # Figure out which features should be continuous + numeric
 # Conert these to the appropriate data type as needed,
 # that is, float64 or int64
-#
+
 # .. your code here ..
+'''
+seems to already be handled?
+'''
 
 
 # TODO:
@@ -27,6 +37,12 @@ import numpy as np
 #
 # .. your code here ..
 
+'''
+ordinal
+- education?
+- age buckets?
+- hour buckets?
+'''
 
 # TODO:
 # Look through your data and identify any potential categorical
@@ -35,6 +51,12 @@ import numpy as np
 #
 # .. your code here ..
 
+'''
+sex?
+'''
 
+df = pd.get_dummies(df, columns=[ 'race', 'sex', 'classification'])
 # TODO:
 # Print out your dataframe
+
+print len(df.columns)
