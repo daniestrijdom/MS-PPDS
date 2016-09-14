81b3f59ab3200a071b9254f97c3fc5eec0140396
daansteraan <d.strijdom@gmail.com>
2016-09-14 16:44:31 +0200

    plotting data

---
+++ Exploring data/scatterplots.py
@@ -0,0 +1,20 @@
+'''
+EXAMPLES FROM MODULE 3 - VISUALISATION
+'''
+
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+# NB - set index_col to 0th col if index already exists in dataset
+df = pd.read_csv("students.data", index_col=0)
+
+series_a = df.G3
+df_a = df[['G3','G2','G1']]
+
+series_a.plot.hist(alpha=0.5)
+plt.show()
+
+df_a.plot.hist(alpha=0.5)
+plt.show()
