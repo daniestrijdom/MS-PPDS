05c4696ea4d884deb63b84db46a52f0941e27c69
daansteraan <d.strijdom@gmail.com>
2016-09-14 13:53:18 +0200

    still cleaning up the data
    palyers are scored by pts/game and ranked

--- barx.py
+++ barx.py
@@ -1,4 +1,6 @@
 import pandas as pd
+pd.options.mode.chained_assignment = None
+import math
 
 keepers = "https://barxffl.com/stats.aspx?page=playerSearch&t=1"
 defenders = "https://barxffl.com/stats.aspx?page=playerSearch&t=2"
@@ -6,29 +8,40 @@ midfielders = "https://barxffl.com/stats.aspx?page=playerSearch&t=3"
 forwards = "https://barxffl.com/stats.aspx?page=playerSearch&t=4"
 
 df_k = pd.read_html(keepers)
-df_p = df_k[0]
+df = df_k[0]
 
 df_d = pd.read_html(defenders)
-df_p = df_p.append(df_d[0],ignore_index=True)
+df = df.append(df_d[0],ignore_index=True)
 
 df_m = pd.read_html(midfielders)
-df_p = df_p.append(df_m[0],ignore_index=True)
+df = df.append(df_m[0],ignore_index=True)
 
 df_f = pd.read_html(forwards)
-df_p = df_p.append(df_f[0],ignore_index=True)
+df = df.append(df_f[0],ignore_index=True)
 
-df = df_p
+# TODO: remove players that have not played and those that have been scoring negative points
 df = df[(df["Pld."]!=0) & (df['Pts.']>0)]
+df = df.fillna(0)
 
-#TODO: points per game & cost per point metrics
+# TODO: points per game & cost per point metrics, rank by ponts per game
 df["pts/g"] = df["Pts."]/df["Pld."]
-df["cost/pt"] = df["Value"]/df["Pts."]/1000000
+df['cost/p'] = df["Value"]/df["Pts."]/1000000
 
-# print df[(df["pts/g"]>df["pts/g"].mean()) & (df["Pld."]==4)]
-print df[df['Pos.']=="K"].sort(columns=["pts/g"], ascending=False).head()
-print df[df['Pos.']=="D"].sort(columns=["pts/g"], ascending=False).head()
-print df[df['Pos.']=="M"].sort(columns=["pts/g"], ascending=False).head()
-print df[df['Pos.']=="S"].sort(columns=["pts/g"], ascending=False).head()
+df =  df[df['Pld.'] >= math.floor(df['Pld.'].mean())].sort_values(by=("pts/g"), ascending=False)
+df = df.reset_index(drop=True)
+
+'''
+print df[(df['Pos.']=="K") & (df['Pld.'] >= math.floor(df['Pld.'].mean()))].sort_values(by=("pts/g"), ascending=False).head()
+print df[(df['Pos.']=="D") & (df['Pld.'] >= math.floor(df['Pld.'].mean()))].sort_values(by=("pts/g"), ascending=False).head()
+print df[(df['Pos.']=="M") & (df['Pld.'] >= math.floor(df['Pld.'].mean()))].sort_values(by=("pts/g"), ascending=False).head()
+print df[(df['Pos.']=="S") & (df['Pld.'] >= math.floor(df['Pld.'].mean()))].sort_values(by=("pts/g"), ascending=False).head()
+'''
+
+print df.head()
+
+print df.tail()
+
+print df.describe()
 
 
 #TODO: test formations to see if less than 100K in value
